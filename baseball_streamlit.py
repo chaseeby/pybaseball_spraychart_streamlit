e2bbb5011e24b24c2c43b478041a00cf0eaa8c49
chaseeby <chaseeby100@gmail.com>
2022-05-22 20:53:52 -0700

    Update baseball_streamlit.py

--- baseball_streamlit.py
+++ baseball_streamlit.py
@@ -56,7 +56,7 @@ stadium = st.sidebar.selectbox(
      'phillies','pirates','rangers','rays','red_sox','reds','rockies','royals','tigers','twins','white_sox','yankees')
      )
 
-st.write('You selected:', dic[option].decode('utf8') + ' MLB ID: ' + str(option))
+st.write('You selected:', dic[option] + ' MLB ID: ' + str(option))
 
 st.dataframe(batting[batting['mlbID'] == option])  # Same as st.write(df)
     
