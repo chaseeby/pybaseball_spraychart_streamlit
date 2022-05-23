599874f2ee4085c275dce1e332709be48c7cd69c
chaseeby <chaseeby100@gmail.com>
2022-05-22 20:34:09 -0700

    Update baseball_streamlit.py

--- baseball_streamlit.py
+++ baseball_streamlit.py
@@ -21,6 +21,7 @@ pybaseball.cache.enable()
 batting = league_batting_stats.batting_stats_range(start_dt=start_dt, end_dt=end_dt)
 batting = league_batting_stats.batting_stats_range(start_dt=start_dt, end_dt=end_dt)
 batter_name_id = batting[['Name','mlbID']]
+batter_name_id['Name']=batter_name_id['Name'].apply(lambda x:x[2:-1].encode().decode("unicode_escape").encode('raw_unicode_escape').decode())
 
 @st.cache
 def convert_df(df):
